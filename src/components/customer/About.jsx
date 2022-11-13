1f293b8ec6dc81527f170bb2dfb25c1f86a9f2ba
Nitisha Gupta <nitishagupta.1999@gmail.com>
2022-11-13 21:19:06 +0530

    implemented about css

--- src/components/customer/About.jsx
+++ src/components/customer/About.jsx
@@ -1,34 +1,49 @@
+import Menu from '../menu/Menu';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import Logout from '../../authentication/Logout';
+import { useUser } from '../../hooks/useUser';
 import useUserInfo from '../../hooks/useUserInfo';
+import '../../styles/About.css';
 
 export default function About() {
     let navigate = useNavigate();
     const info = useUserInfo();
+    const { token } = useUser();
 
     return (
         <>
-            <div className="about">Personal Details</div>
-            <button onClick={() => navigate("/menu")}>Home</button>
-            <div>
-                {
+            <div className="menu-navigation">
+                <div id="menu-heading" onClick={() => navigate("/menu")} style={{ cursor: "pointer" }}><img src={"/logo192.png"}></img>Yolo's Pizza</div>
+                <div className="menu-navigation-right">
+                    <button onClick={() => navigate("/menu")}>Menu</button>
+                    {!token && <button id="menu-login-button" onClick={() => navigate("/login?from=/cart")}>Login</button>}
+                    {!token && <button id="menu-signup-button" onClick={() => navigate("/register")}>Signup</button>}
+                    {token && <button id="menu-orders-button" onClick={() => navigate("/orders")}>Orders</button>}
+                    <Logout />
+                </div>
+            </div>
+            <div className='about-container'>
+                <div className="about-personal-details-heading">Personal Details</div>
+                {token &&
                     (() => {
                         const { customerName, customerMobile, customerEmail, customerAddress, username } = info;
                         return (
-                            <article>
-                                <div className="name">Name : {customerName}</div>
-                                <div className="username">Username : {username}</div>
-                                <div className="mobile">Mobile no. : {customerMobile}</div>
-                                <div className="email">Email : {customerEmail}</div>
-                                <div className="address">Address : {customerAddress}</div>
-                                <button onClick={() => navigate("/user/update")}>Update credentials</button>
-                                <button onClick={() => navigate("/user/update_uname")}>Update username</button>
-                                <button onClick={() => navigate("/user/update_pass")}>Update password</button>
+                            <article className='about-card'>
+                                <div className="about-customer-name"><span id='about-span'>Name :</span>{customerName}</div>
+                                <div className="about-customer-username"><span id='about-span'>Username :</span>{username}</div>
+                                <div className="about-customer-mobile"><span id='about-span'>Mobile no. :</span>{customerMobile}</div>
+                                <div className="about-customer-email"><span id='about-span'>Email :</span>{customerEmail}</div>
+                                <div className="about-customer-address"><span id='about-span'>Address :</span>{customerAddress}</div>
+                                <button className='about-update-credentials' onClick={() => navigate("/user/update")}>Update credentials</button>
+                                <button className='about-update-username' onClick={() => navigate("/user/update_uname")}>Update username</button>
+                                <button className='about-update-password' onClick={() => navigate("/user/update_pass")}>Update password</button>
                             </article>
                         )
                     })()
                 }
             </div>
+            {!token && navigate("/menu")}
         </>
     )
 }
